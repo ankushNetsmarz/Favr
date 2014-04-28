c23f0c37d29c2142439844398e0cac12209c6799
ankushNetsmarz <ankush.sharma@netsmartz.net>
2014-04-28 18:13:18 +0530

    Initial Commit

---
+++ Favr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Favr
+//
+//  Created by Weexcel on 28/04/14.
+//  Copyright (c) 2014 Netsmartz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
