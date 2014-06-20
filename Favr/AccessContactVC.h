299e67df08735d501dda8d718e6ae81e77fc1215
taranjitsingh <taranjit.sngh@gmail.com>
2014-06-21 03:53:13 +0530

    Things Added/Modified First Page Get Default Email (FB) Linkedin Twitter Landing Page Favr Page Pending Page Contacts Page

--- Favr/AccessContactVC.h
+++ Favr/AccessContactVC.h
@@ -8,11 +8,26 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol AccessFBDelegate <NSObject>
+
+-(void)accessedUserFBAccountSuccessfully:(BOOL)success;
+
+@end
+
+
 @interface AccessContactVC : NSObject
 @property(nonatomic,strong)NSArray* userContacts;
 
 +(id)sharedManager;
 
+@property(nonatomic,strong)NSString* userFBEmail;
+@property(nonatomic,strong)NSString* userFBName;
+@property(nonatomic,strong)UIImage* userFBImage;
+@property(nonatomic,strong)NSString* userFBID;
+@property(nonatomic,strong)NSString* fbAccessToken;
+@property(nonatomic, weak)id<AccessFBDelegate> delegate;
 -(void)fetchContacts;
 -(void)inviteNonFriends;
+- (void)requestFacebookAccessFirstTime;
+
 @end
